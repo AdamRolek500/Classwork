ec36421d66a692a2cb5f9e86cc7b5a9a8fa51e5d
Unknown <adamrolek500@gmail.com>
2020-02-11 00:39:54 -0500

    Playing with the HTML templates and replaced them for the buttons

--- javascript/calendar/main.js
+++ javascript/calendar/main.js
@@ -1,14 +1,10 @@
 function loadCalendar() {
     let calTable = document.getElementById("calTable");
-
     let today = new Date(), y = today.getFullYear(), m = today.getMonth();
     let dateForString = new Date(y, m, 1);
     let indexOfFirstDay = new Date(today.getFullYear(), today.getMonth(), 1).getDay();
     let numOfDaysInMonth = new Date(today.getFullYear(), today.getMonth() + 1, 0).getDate();
 
-
-
-    console.log(dateForString);
 
     // Inserting empty rows here
     let row = calTable.insertRow();
@@ -22,35 +18,21 @@ function loadCalendar() {
             row = calTable.insertRow();
         }
         let cell = row.insertCell();
-
-        let btn = document.createElement('input');
-        btn.type = "button";
-        btn.className = "btn";
-        btn.value = i - indexOfFirstDay + 1;
-        btn.setAttribute("data-date", dateForString.toDateString());
-        btn.setAttribute("onClick", "addEvent(this.getAttribute('data-date'))");
-        cell.appendChild(btn);
+        let temp = document.getElementById("calPane");
+        let pane = temp.content.cloneNode(true);
+        pane.getElementById("paneDate").innerHTML = i - indexOfFirstDay + 1;
+        pane.getElementById("paneDate").setAttribute("id", dateForString.toDateString());
+        pane.getElementById("paneDiv").setAttribute("id", dateForString.toDateString());
+        cell.appendChild(pane);
         dateForString.setDate(dateForString.getDate() + 1);
     }
-
-    // let i;
-    // let j;
-    // for (i = 0; i < numOfDaysInMonth; i += j) {
-    //     let row = calTable.insertRow();
-    //     for (j = 0; j < 7 && i + j < numOfDaysInMonth; j++) {
-    //         let cell = row.insertCell();
-    //         cell.innerHTML = i + j + 1;
-    //     }
-    // }
-
-
-    let temp = document.getElementById("calPane");
-    let clon = temp.content.cloneNode(true);
-    clon.getElementById("paneDate").innerHTML = today.toDateString();
-    let cell = row.insertCell();
-    cell.appendChild(clon);
 }
 
-function addEvent(date) {
-    alert(date)
+function addEvent(event) {
+    alert(event.target.id);
+}
+
+function test(event) {
+    event.stopPropagation();
+    alert("qwerty");
 }
