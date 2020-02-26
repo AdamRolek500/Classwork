e52cf6cd8230b753bda25bccfc74b74c585c8544
AdamRolek500 <adamrolek500@gmail.com>
2020-02-26 14:12:13 -0500

    Finished calendar

--- javascript/calendar_for_grade/main.js
+++ javascript/calendar_for_grade/main.js
@@ -1,11 +1,49 @@
-function loadCalendar() {
+window.dateNS = {};
+function loadCalendar(act) {
+    try {
+        // Adding and subtracting based on input
+        if (act === '>') {
+            dateNS.month = dateNS.month + 1;
+        } else if (act === '>>') {
+            dateNS.year = dateNS.year + 1;
+        } else if (act === '<') {
+            dateNS.month = dateNS.month - 1;
+        } else if (act === '<<') {
+            dateNS.year = dateNS.year - 1;
+        }
+    } catch (e) {
+        console.warn("The calendar has not been generated yet!");
+        console.log("Generating the calendar for today instead.");
+        act = 'T';
+    }
+
     let calTable = document.getElementById("calTable");
-    let today = new Date(), y = today.getFullYear(), m = today.getMonth();
-    let indexOfFirstDay = new Date(today.getFullYear(), today.getMonth(), 1).getDay();
-    let numOfDaysInMonth = new Date(today.getFullYear(), today.getMonth() + 1, 0).getDate();
+    let date;
+    if (act === 'T') {
+        date = new Date();
+    } else {
+        date = new Date(dateNS.year, dateNS.month);
+    }
+    let y = date.getFullYear();
+    let m = date.getMonth();
+    let indexOfFirstDay = new Date(date.getFullYear(), date.getMonth(), 1).getDay();
+    let numOfDaysInMonth = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate();
+
+    // Creating my own namespace to store values
+    dateNS.month = m;
+    dateNS.year = y;
+    if (act === 'T') {
+        dateNS.todayDay = date.getDate();
+        dateNS.todayMonth = m;
+        dateNS.todayYear = y;
+    }
+
+    // Setting the calendar header
+    let months = ["Jan", "Feb", "Mar", "April", "May", "Jun", "July", "Aug", "Sep", "Oct", "Nov", "Dec"];
+    document.getElementById("calHeader").innerHTML = months[dateNS.month] + " / " + dateNS.year;
 
     // Make sure there is nothing in the table
-    while(calTable.rows.length > 1) {
+    while(calTable.rows.length > 2) {
         calTable.deleteRow(calTable.rows.length - 1);
     }
 
@@ -19,22 +57,20 @@ function loadCalendar() {
 
     // Inserting the rows for the days of the month
     for (i = indexOfFirstDay; i < numOfDaysInMonth + indexOfFirstDay; i++) {
+        // Inserting a new row for every week
         if (i % 7 === 0) {
             row = calTable.insertRow();
         }
         let cell = row.insertCell();
         // This if/else will make sure the current day is highlighted
-        if (i - indexOfFirstDay + 1 === today.getDate()){
+        if (i - indexOfFirstDay + 1 === dateNS.todayDay && m === dateNS.todayMonth && y === dateNS.todayYear){
             cell.setAttribute("class", "today");
         } else if (i % 7 === 0 || i % 7 === 6) {
             cell.setAttribute("class", "weekend");
-            console.log("HEY");
         } else {
             cell.setAttribute("class", "notToday");
         }
-        console.log(i);
         // Setting the innerHTML to the date
         cell.innerHTML = i - indexOfFirstDay + 1;
     }
 }
-
